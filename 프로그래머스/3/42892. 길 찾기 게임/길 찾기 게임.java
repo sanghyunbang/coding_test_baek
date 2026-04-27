27e84929abad2dce2b9523598d1776bf6f5524fd
BANG SANGHYUN <149238724+sanghyunbang@users.noreply.github.com>
2026-04-27 12:57:27 +0900

    [level 3] Title: 길 찾기 게임, Time: 35.20 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,9 +1,8 @@
 import java.util.*;
 
 class Solution {
-    public class Node{
-        int id;
-        int x, y;
+    class Node{
+        int id, x, y;
         Node left, right;
         
         Node(int id, int x, int y){
@@ -13,105 +12,95 @@ class Solution {
         }
     }
     
-    public List<Node> list;
-    public int[][] answer;
     public int idx;
+    public int[][] answer;
+    public List<Node> list;
     
     public int[][] solution(int[][] nodeinfo) {
         
+        // 0. 일단 y축 기준으로 정렬, y 축 같으면 x축 기준으로 정렬해보기
         list = new ArrayList<>();
         
-        // 0. 일단 집어 넣기
-        for(int i = 0; i < nodeinfo.length; i++){
+        // 일단은 리스트에 다 집어 넣기
+        for (int i = 0; i < nodeinfo.length; i++){
             Node node = new Node(i+1, nodeinfo[i][0], nodeinfo[i][1]);
             list.add(node);
         }
         
-        // 1. 정렬하기 : y축 기준으로 내림차순인데 만약에 y가 같으면 x 기준 오름차순
+        // 리스트 정렬
         list.sort((s1, s2) -> {
-                
-                if (s1.y == s2.y) {
-                    return Integer.compare(s1.x, s2.x); // y같으면 x는 오름차순
+                    if (s1.y == s2.y){
+                        return Integer.compare(s1.x, s2.x);
+                    }
+                    return Integer.compare(s2.y, s1.y);
                 }
-                return Integer.compare(s2.y, s1.y);
-            }
         );
-        
-        // 2. 이제 여기에서 트리 만들기 : 각각의 Node에 left, right 넣어주는 것
-        Node root = list.get(0); // y가 가장 큰 게 지금 가장 앞에 나와 있는 상황
-        
-        // 하나하나 누구의 자식인지 채워 넣는 방식으로 가야 함
+
+        // 1. 이제 Node 별로 left, right에 값 넣어서 트리 연결 
+        // 루트로부터 타고 들어가면서 해당 노드가 누구네 자식인지 확인하고 넣기
         for (int i = 1; i < list.size(); i++){
-            insertNode(root, list.get(i));
+            makeTree(list.get(0), list.get(i));
         }
         
-        // 이제 여기에서 부터 전위/후위 순회 하기
+        
+        // 이제 트리들도 다 채워졌으니 전위 순회, 후위 순회 하기
         answer = new int[2][nodeinfo.length];
-        
-        // 전위 순회
+ 
         idx = 0;
-        preorder(root);
+        preorder(list.get(0));
         
-        // 후위 순회
         idx = 0;
-        postorder(root);
+        postorder(list.get(0));
         
-        // 이제 프린트
+        
         return answer;
-        
     }
     
-    public void preorder(Node root){
-        // 일단 넣고 시작
+    public void preorder(Node root){        
+        // 일단 채워넣기
         answer[0][idx] = root.id;
-        idx++; // 인덱스 이제 업로드
-        // 이제 왼쪽
-        if(root.left != null){
+        idx++; // 채우고 나면 idx올리기
+        
+        
+        // 왼쪽 자식이 있으면 왼쪽 으로 가기
+        if(root.left != null) {
             preorder(root.left);
         }
-        // 이제 오른쪽
+        
+        // 오른쪽 자식이 있으면 오른쪽으로 가기
         if(root.right != null){
             preorder(root.right);
         }
-        // 만약에 자식이 null인 상황이면 자기만 넣고 끝나버림
     }
     
-    public void postorder(Node root){
-        
-        // 왼쪽부터 살펴보고, 왼쪽이 없는 경우에만 업데이트
-        if(root.left != null){
-            postorder(root.left);
-        } 
-        
-        // 이제 오른쪽
-        if(root.right != null){
-            postorder(root.right);
+    public void postorder(Node node){ 
+        if(node.left != null){
+            postorder(node.left);
         }
         
-        // 위에서 왼쪽끝 오른쪽 끝까지 쭉 간다음에 이제 해당 값 넣기
-        // 이러면 위에 의해서 맨 왼쪽, 오른쪽 다 거치고 나서야 내가 들어감
-        answer[1][idx] = root.id;
-        idx++; // 인덱스 이제 업로드
+        if(node.right != null){
+            postorder(node.right);
+        }
+        
+        answer[1][idx] = node.id;
+        idx++;        
     }
     
-    public void insertNode(Node parent, Node child){
-        // 왼/오 여부 부터
-        if(child.x < parent.x){
-            // 여기에서 왼쪽 자식이 없으면 넣고 있으면 그 아래 자식한테로 보내기
-            if (parent.left == null){
+    public void makeTree(Node parent, Node child){
+        // 일단 left인지 right인지 부터 확인
+        // 왼쪽 자식의 경우
+        if (parent.x > child.x){
+            if(parent.left == null){
                 parent.left = child;
             } else {
-                insertNode(parent.left, child);
+                makeTree(parent.left, child);
             }
         } else {
-            // 여기에서 오른쪽 자식이 없으면 넣고 있으면 그 아래 자식한테로 보내기
-            if (parent.right == null){
+            if(parent.right == null){
                 parent.right = child;
             } else {
-                insertNode(parent.right, child);
+                makeTree(parent.right, child);
             }
         }
     }
-    
-    
 }
