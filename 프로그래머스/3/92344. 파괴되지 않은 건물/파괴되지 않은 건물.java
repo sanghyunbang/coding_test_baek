2a3b4c95470c96747b162a5a522262ad89fc2fc3
BANG SANGHYUN <149238724+sanghyunbang@users.noreply.github.com>
2026-04-27 16:01:49 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 59.47 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,56 +1,53 @@
 class Solution {
-    public int solution(int[][] board, int[][] skill) {
+    public int solution(int[][] board, int[][] skills) {
         
-        int m = board.length;
-        int n = board[0].length;
+        int rNum = board.length;
+        int cNum = board[0].length;
         
-        int[][] padding = new int[m+1][n+1];
+        int[][] padding = new int[rNum + 1][cNum + 1];
         
-        for (int[] sk : skill){
-            int type = sk[0];
-            int r1 = sk[1];
-            int c1 = sk[2];
-            int r2 = sk[3];
-            int c2 = sk[4];
-            int v = sk[5];
+        for (int[] skill : skills){
+            
+            int type = skill[0];
+            int fr = skill[1];
+            int fc = skill[2];
+            int er = skill[3];
+            int ec = skill[4];
+            int v = skill[5];
             
             if (type == 1) v = -v;
             
-            padding[r1][c1] += v;
-            padding[r1][c2 + 1] -= v;
-            padding[r2 + 1][c1] -= v;
-            padding[r2 + 1][c2 + 1] += v;
+            padding[fr][fc] += v;
+            padding[fr][ec + 1] -= v;
+            padding[er + 1][fc] -= v;
+            padding[er + 1][ec + 1] += v;
         }
         
-        // 칼럼방향 : -> 으로 누적합 
-        for(int r = 0; r <= m; r++){
-            for(int c = 1; c <= n; c++){
-                padding[r][c] += padding[r][c-1];
+        // 가로 누적합
+        for (int r = 0; r <= rNum; r++){
+            int sum = padding[r][0];
+            for (int c = 1; c <= cNum; c++){
+                sum += padding[r][c];
+                padding[r][c] = sum;
             }
         }
         
-        // 로우방향 : (위아래) 으로 누적합 
-        for(int r = 1; r <= m; r++){
-            for(int c = 0; c <= n; c++){
-                padding[r][c] += padding[r-1][c];
+        // 세로 누적합
+        for (int c = 0; c <= cNum; c++){
+            int sum = padding[0][c];
+            for (int r = 1; r <= rNum; r++){
+                sum += padding[r][c];
+                padding[r][c] = sum;
             }
         }
         
-        // board와 padding더하기
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                board[i][j] += padding[i][j];
+        int ans = 0;
+        for (int r = 0; r < rNum; r++){
+            for (int c = 0; c < cNum; c++){
+                if (board[r][c] + padding[r][c] > 0) ans++;
             }
         }
         
-        // 답 구하기
-        int answer = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if (board[i][j] > 0) answer++;
-            }
-        }        
-        
-        return answer;
+        return ans;
     }
 }
