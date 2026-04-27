0a208e73ac63d918fac966916171326227de0f51
BANG SANGHYUN <149238724+sanghyunbang@users.noreply.github.com>
2026-04-27 21:14:43 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.96 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -3,104 +3,69 @@ import java.util.*;
 class Solution {
     public long solution(String expression) {
         
-        char[][] ops = new char[6][3];
-        ops[0] = new char[]{'*', '+', '-'};
-        ops[1] = new char[]{'*', '-', '+'};
-        ops[2] = new char[]{'+', '*', '-'};
-        ops[3] = new char[]{'+', '-', '*'};
-        ops[4] = new char[]{'-', '*', '+'};
-        ops[5] = new char[]{'-', '+', '*'};
+        char[][] opcc = {
+            {'+', '-', '*'}, {'+', '*', '-'},
+            {'-', '+', '*'}, {'-', '*', '+'},
+            {'*', '+', '-'}, {'*', '-', '+'}
+        };
         
+        List<Long> nums = new ArrayList<>();
+        List<Character> ops = new ArrayList<>();
+        
+        // 처음에 숫자들
+        int n = expression.length();
+        
+        // [ex]. 12 + 13 -> 12 + --> 근데 뒤에 13은?
+        StringBuilder num = new StringBuilder();
+        for (char c : expression.toCharArray()){
+            if (c == '+' || c == '-' || c == '*'){
+                ops.add(c);
+                nums.add(Long.parseLong(num.toString()));
+                num.setLength(0);
+            } else {
+                num.append(c);    
+            }
+        }
+        nums.add(Long.parseLong(num.toString()));
+        
+        // 1. 여기서부터 이제 코드 시작
+        
+
         long answer = 0;
         
-        for(char[] op : ops){
-            
-            // 일단 가장 첫번째 부터
-            String s1 = calculated(expression, op[0]);
-            String s2 = calculated(s1, op[1]);
-            String s3 = calculated(s2, op[2]);
-            
-            // s3를 long으로 만들고 업데이트
-            answer = Math.max(answer, Math.abs(Long.parseLong(s3)));
+        for(char[] cc : opcc){
+            List<Long> numC = new ArrayList(nums);
+            List<Character> opsC = new ArrayList(ops);
+            for (char c : cc){
+                calc(numC, opsC, c);               
+            }
+            answer = Math.max(answer, Math.abs(numC.get(0)));
         }
-        
         return answer;
     }
     
-    public String calculated(String s, char c){
+    public void calc(List<Long> numC, List<Character> opsC, char c){
         
-        int n = s.length();
-        
-        boolean exist = false;
-        for (int i = 0; i < n; i++){
-            if (s.charAt(i) == c) exist = true;
-        }
-        
-        if(!exist) return s;
-        
-        List<String[]> st = new ArrayList<>();
-            
-        // c에 해당하면 ^이걸로 바꿔놓기
-        s = s.replace(c, '^');
-        
-        List<String> vals = new ArrayList<>();
-        List<Character> op = new ArrayList<>();
-        
-        int start = 0;
-        for (int i = 0; i < n; i++){
-            if(s.charAt(i) == '+' || s.charAt(i) == '*'){
-                vals.add(s.substring(start, i));
-                op.add(s.charAt(i));
-                start = i+1;
-            } else if (s.charAt(i) == '-') {
-                if (i == 0) continue;
-                if (s.charAt(i-1) == '+' || s.charAt(i-1) == '*'|| s.charAt(i-1) == '^') continue;
-                
-                vals.add(s.substring(start, i));
-                op.add(s.charAt(i));
-                start = i+1;
+        // 100 200 300 500 20
+        // 100 500 500 20
+        for (int i = 0; i < opsC.size(); i++){
+            if (opsC.get(i) == c){
+                long v = helper(numC.remove(i), numC.remove(i), c);
+                numC.add(i, v);
+                opsC.remove(i);
+                i--; // 해당 인덱스 이제 다시 확인해야 하는데 -> 여기서 빼야 위로 올라가서 ++ 해서 원래 idx
             }
         }
-        // 연산 앞쪽만 들어가서 뒤쪽은 추가로 또 더해줘야
-        vals.add(s.substring(start));
-        
-        // 이제 vals값 업데이트하기
-        for (int id = 0; id < vals.size(); id++) {
-            String val = vals.get(id);
-            
-            for (int i = 0; i < val.length(); i++){
-                
-                if (val.charAt(i) == '^'){
-                    String[] ss = val.split("\\^");
-                    Long former = Long.parseLong(ss[0]);
-                    Long newV = former;
-                    
-                    if ( c == '+') {
-                        for (int k = 1; k < ss.length; k++){
-                            newV += Long.parseLong(ss[k]);
-                        }
-                    } else if (c == '-') {
-                        for (int k = 1; k < ss.length; k++){
-                            newV -= Long.parseLong(ss[k]);
-                        }
-                    } else {
-                        for (int k = 1; k < ss.length; k++){
-                            newV *= Long.parseLong(ss[k]);
-                        }
-                    }
-                    vals.set(id, newV.toString());                    
-                    break;
-                }
-            }
-        }
-        
-        // 여기에서 합쳐서 출력해주기
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < op.size(); i++){
-            sb.append(vals.get(i)).append(op.get(i));
-        }
-        sb.append(vals.get(vals.size() - 1));
-        
-        return sb.toString();
     }
+    
+    public long helper(long a, long b, char c){
+        if (c == '+'){
+            return a + b;
+        } else if (c == '-'){
+            return a - b;
+        } else {
+            return a * b;
+        }
+    }
+    
 }
