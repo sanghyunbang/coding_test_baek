459adcc8decbb8e527f20e066eae793fae5c42c6
BANG SANGHYUN <149238724+sanghyunbang@users.noreply.github.com>
2026-04-29 11:09:10 +0900

    Time: 0 ms (100%), Space: 48.3 MB (50.32%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int answer = -1;
+
+        int n = nums.length;
+        int l = 0;
+        int r = n - 1;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return answer;
+
+
+    }
+}
